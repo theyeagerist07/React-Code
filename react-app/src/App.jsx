57305c5a57475a921f4234089ae29574613c7fb5
theyeagerist07 <theyeagerist07@gmail.com>
2024-11-24 06:27:43 +0600

    Setup & Basic Discuss Done

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -4,7 +4,8 @@ export default function App() {
     <h1 className="text-3xl font-bold underline justify-center text-center bg-pink-300 p-12 text-slate-700">
       Hello world!
     </h1>
-    <h2 className="font-mono text-center bg-cyan-600 text-2xl">This React Course Is Awesome</h2>
+    <h2 className="font-mono text-center bg-cyan-600 text-2xl text-yellow-50">This React Course Is Awesome</h2>
+    <h3 className="bg-purple-700 text-2xl text-center text-white p-12 ">Welcome to React Course</h3>
     </div>
   )
 }
